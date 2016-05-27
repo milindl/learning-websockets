a249cd8d195fd3d22abc791975a10e2f3cae89e1
Milind L <milindl@iitk.ac.in>
2016-05-27 17:40:06 +0100

    Can send simple messages now with the sending side of protocol sufficiently well implemented

--- server.py
+++ server.py
@@ -21,7 +21,47 @@ Sec-WebSocket-Accept: '''
         reply=reply+websocket_accepted_key+"\r\n\r\n"
         sock.send(reply.encode())
         print(reply)
-        sock.sendall('')
+        payload = bytearray()
+        data=('hello').encode('utf-8')
+        payload.append(0b10000001)
+        payload.append(len(data))
+        #payload.append(b1)
+        #payload.append(b2)
+        payload.extend(data)
+        print(payload)
+        self.send_data("o"*8000*2, sock)
+
+    def send_data(self, data, sock):
+        '''
+        Prepare the headers and send the message. The sock in the params in temporary. Final solution is to use send queue
+        '''
+        data = data.encode('utf-8')
+        payload=bytearray()
+        length = len(data)
+        s1=0b10000001
+        #Flg:fin,rsv1,rsv2,rsv3,opcode
+        s2=0b0
+        # flg:mask
+        #Possible thingies:
+        #   Length less than 125
+        #   Length more than 125 but less than 16 bit
+        #   Length more than 16bit(rare)
+        payload.append(s1)
+        if length<=125:
+            payload.append(s2+length)
+        elif length.bit_length()<=16:
+            payload.append(s2+126)
+            import struct
+            payload.extend(struct.pack("!H", length))  # This tricky bit changes the length 2 byte int to 2 bytes to extend the barray
+        elif length.bit_length()<=64:
+            payload.append(s2+127)
+            import struct
+            payload.extend(struct.pack("!Q", length))  # This changes int->8bytes to add to the bytearray
+        # print(payload)
+        payload.extend(data)
+        # print(payload)
+        sock.send(payload)
+
 
     def parse_headers(self,headers):
         '''
